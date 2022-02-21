314a0e654bd4522f23b282372b19f06b0ca5638f
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-02-22 00:34:18 +0530

    flip coin program uploaded

---
+++ src/com/blz/basicprograms/FlipCoin.java
@@ -0,0 +1,34 @@
+package com.blz.basicprograms;
+
+import java.util.Scanner;
+
+public class FlipCoin {
+
+    public static void main(String[] args) {
+        System.out.println("How many number of times a coin to flip?");
+        Scanner input = new Scanner(System.in);
+        int flips = input.nextInt();
+
+        int heads = 0, tails = 0,coin=1;
+        float HeadsPercentage,TailsPercentage;
+
+        while (coin<=flips) {
+            double random = Math.random();
+            //System.out.println(coin);
+            if (random < 0.5) {
+                tails++;
+                System.out.println("It is tails");
+            } else {
+                heads++;
+                System.out.println("It is heads");
+            }
+            coin++;
+        }
+        System.out.println("Heads flipped "+heads+" times and Tails flipped "+tails+" times");
+        HeadsPercentage=(float)((heads*100)/flips);
+        TailsPercentage=(float)((tails*100)/flips);
+        System.out.println("Heads percentage is "+HeadsPercentage+"");
+        System.out.println("Tails percentage is "+TailsPercentage+"");
+
+    }
+}

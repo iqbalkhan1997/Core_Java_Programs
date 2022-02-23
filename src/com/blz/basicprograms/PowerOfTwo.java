2e7fbceab17e505b6de8c8dda7e9a6d3e68d003d
iqbalkhan1997 <98506534+iqbalkhan1997@users.noreply.github.com>
2022-02-23 08:25:27 +0530

    Update PowerOfTwo.java
    Added: range functionality

--- src/com/blz/basicprograms/PowerOfTwo.java
+++ src/com/blz/basicprograms/PowerOfTwo.java
@@ -3,12 +3,16 @@ package com.blz.basicprograms;
 public class PowerOfTwo {
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
-        System.out.println("Enter 2 to the power value");
+        System.out.println("Enter the value from 0 to 30 to calculate 2 to the power value");
         int N=input.nextInt();
-
+    if(N<=30){
         for(int i=0;i<=N;i++){
             int poWer=(int)(Math.pow(2,i));
             System.out.println("2 power "+i+" is "+ poWer);
         }
-    }
+        else{
+         System.out.println("Please enter the value between 0 to 30");
+        }
+      }
+   }     
 }
